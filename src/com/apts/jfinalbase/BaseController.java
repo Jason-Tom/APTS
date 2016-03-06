ce4e0015a1807f587436ab35fe4235429984c0e1
ni_menhao <ni_menhao@sina.com>
2016-03-06 20:01:40 +0800

    ceshi

--- src/com/apts/jfinalbase/BaseController.java
+++ src/com/apts/jfinalbase/BaseController.java
@@ -3,8 +3,8 @@ package com.apts.jfinalbase;
 import com.jfinal.core.Controller;
 
 public abstract class BaseController extends Controller {
-	public void index(){
-		
+	public void index() {
+
 	}
-	
+
 }
