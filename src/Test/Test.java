c1a51e9fd703e96b826731aa122ca132791ac625
ni_menhao <ni_menhao@sina.com>
2016-03-06 13:29:37 +0800

    清除测试代码

--- src/Test/Test.java
+++
@@ -1,8 +0,0 @@
-package Test;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("abcdefg-----------------------------------------------123");
-		System.out.println("hahah");
-	}
-}
