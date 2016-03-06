21bc6bf24153e37c66a7ac3b01d11f897b08cf9a
ni_menhao <ni_menhao@sina.com>
2016-03-06 13:27:04 +0800

    测试

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,5 +3,6 @@ package Test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("abcdefg-----------------------------------------------123");
+		System.out.println("hahah");
 	}
 }
