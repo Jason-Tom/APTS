41accef2bbc0f8ad815d05703a6e3af971d06ed5
ni_menhao <ni_menhao@sina.com>
2016-03-06 14:18:59 +0800

    搭建环境

---
+++ src/com/apts/jfinalbase/BaseConfig.java
@@ -0,0 +1,37 @@
+package com.apts.jfinalbase;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+public class BaseConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants me) {
+
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+
+	}
+
+}
