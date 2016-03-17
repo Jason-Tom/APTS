bf8648b94b82fa8108660aa2e4967cf18d633238
Jason-Tom <429295712@qq.com>
2016-03-17 14:11:35 +0800

    问题应该是与数据库方面有关

--- src/com/apts/controller/IndexController.java
+++ src/com/apts/controller/IndexController.java
@@ -8,8 +8,8 @@ public class IndexController extends BaseController {
 	public void index() {
 		//操作数据库
 //		new TbAdmin().set("account", "haha").set("password", "123456").set("pwdsalt", "eeee").save();//插入数据
-		this.renderText( TbAdmin.dao.findById(1).get("account").toString() );//读取数据
-//		this.renderText("aa" );
+		this.renderText( TbAdmin.dao.findById(1).get("account").toString() );//读取数据,(审核正确)
+		//		this.renderText("aa" );
 		
 	}
 }
