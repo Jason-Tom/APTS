61d6591f237f7d1efbf357bf9535265386028e23
Jason-Tom <429295712@qq.com>
2016-03-17 17:17:58 +0800

    增 删 查 改

--- src/com/apts/controller/IndexController.java
+++ src/com/apts/controller/IndexController.java
@@ -7,9 +7,10 @@ public class IndexController extends BaseController {
 
 	public void index() {
 		//操作数据库
-		new TbAdmin().set("account", "haha1").set("password", "123456").set("pwdsalt", "eeee").save();//插入数据
+//		new TbAdmin().set("password", "123456").set("pwdsalt", "eeee").save();//插入数据，（小心数据不能重复）
 //		this.renderText( TbAdmin.dao.findById(1).get("account").toString() );//读取数据,(审核正确)
 		//		this.renderText("aa" );
-		
+//		new TbAdmin().dao.deleteById(5); //删除id值为5的记录
+//		new TbAdmin().dao.findById(1).set("account", "James").update(); //修改数据，是findById，官方文档有错	
 	}
 }
